cfc68d080503577a1e3684523e82f8d22d425427
miguel-hidsan <miguelhidsan@gmail.com>
2022-01-26 20:01:40 +0100

    email

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -1,58 +1,75 @@
-import React from 'react'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-function RSVP () {
+function RSVP() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_9n8pqsr",
+        "template_36bf7tj",
+        form.current,
+        "user_lH7VuA9l059MGRrJfFqPW"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+    alert("Mensaje enviado");
+  };
+
   return (
-    <div
-      id='rsvp'
-      className='section-padding bg-img bg-fixed'
-    >
-      <div className='container'>
-        <div className='row justify-content-center'>
-          <div className='col-md-6 bg-white p-40'>
-            {' '}
-            <span className='oliven-title-meta text-center'>¿Asistirás?</span>
-            <h2 className='oliven-title text-center'>¿Contamos contigo?</h2>
+    <div id="rsvp" className="section-padding bg-img bg-fixed">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6 bg-white p-40">
+            {" "}
+            <span className="oliven-title-meta text-center">¿Asistirás?</span>
+            <h2 className="oliven-title text-center">¿Contamos contigo?</h2>
             <br />
-            <form method='post' className='row'>
-              <div className='col-md-12'>
-                <div className='form-group'>
+            <form ref={form} onSubmit={sendEmail} className="row">
+              <div className="col-md-12">
+                <div className="form-group">
                   <input
-                    type='text'
-                    className='form-control'
-                    placeholder='Name'
+                    type="text"
+                    className="form-control"
+                    placeholder="Nombre"
+                    name="user_name"
                     required
-                  />{' '}
+                  />{" "}
                 </div>
               </div>
-              <div className='col-md-12'>
-                <div className='form-group'>
-                  <input type='text' className='form-control' placeholder='Email' />{' '}
-                </div>
-              </div>
-              <div className='col-md-12'>
-                <div className='form-group'>
+              <div className="col-md-12">
+                <div className="form-group">
                   <input
-                    type='text'
-                    className='form-control'
-                    placeholder='Guests'
-                  />{' '}
+                    type="email"
+                    className="form-control"
+                    placeholder="Email"
+                    name="user_email"
+                  />{" "}
                 </div>
               </div>
-              <div className='col-md-12'>
-                <div className='form-group'>
+              <div className="col-md-12">
+                <div className="form-group">
                   <textarea
-                    name='message'
-                    id='message'
-                    cols='30'
-                    rows='7'
-                    className='form-control'
-                    placeholder='Message'
+                    name="message"
+                    id="message"
+                    cols="30"
+                    rows="7"
+                    className="form-control"
+                    placeholder="Mensaje"
                   ></textarea>
                 </div>
               </div>
-              <div className='col-md-12'>
-                <div className='form-group'>
-                  <input type='submit' className='btn buttono' value='SEND' />{' '}
+              <div className="col-md-12">
+                <div className="form-group">
+                  <input type="submit" className="btn buttono" value="Enviar" />{" "}
                 </div>
               </div>
             </form>
@@ -60,7 +77,6 @@ function RSVP () {
         </div>
       </div>
     </div>
-  )
+  );
 }
-
-export default RSVP
+export default RSVP;
